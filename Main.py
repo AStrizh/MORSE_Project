78376a87aaf4546d13d1ccf46c73d255366dce4c
Aleksandr Strizhevskiy <abstrizhevskiy@gmail.com>
2018-11-15 03:49:35 -0500

    Began python testing

--- Main.py
+++ Main.py
@@ -1,8 +1,42 @@
+import statistics
+import datetime
+import Trade
+
+
 def main():
-    filename = input("Input filename:")
+
+    # time = datetime.datetime.strptime("2017-04-25T17:11:55", "%Y-%m-%dT%H:%M:%S")
+    # print(time)
+    # trade = Trade.Trade(time, 15, 27)
+    #
+    # print(trade.time)
+    # print(trade.price)
+    # print(trade.quantity)
+
+    filename = "samplefile.txt" #input("Input filename:")
     with open(filename) as infile:
         for line in infile:
-            print(line, end='')
+
+            tradeline = line.split("Z")
+            time = datetime.datetime.strptime(tradeline[0], "%Y-%m-%dT%H:%M:%S")
+            trades = tradeline[1].strip().split(" ")
+            trade = Trade.Trade(time, trades[0], trades[1])
+
+            print(trade.time)
+            print(trade.price)
+            print(trade.quantity)
+            print("-----------------")
+
+    # print(time)
+    # print(time + datetime.timedelta(days=3, hours=5, weeks=2))
+    # print()
+    # print(datetime.datetime.now())
+
+    # data = [2.75, 1.75, 1.25, 0.25, 0.5, 1.25, 3.5]
+    # print("The mean is ", statistics.mean(data))
+    # print("The standard deviation is ", statistics.stdev(data))
+    # print("The median is ", statistics.median(data))
+    # print("The max is ", str(max(data)))
 
 
 main()
---
+++ Trade.py
@@ -0,0 +1,21 @@
+import datetime
+
+
+class Trade:
+
+    def __init__(self, time, price, quantity):
+        self._time = time
+        self._price = price
+        self._quantity = quantity
+
+    @property
+    def time(self):
+        return self._time
+
+    @property
+    def price(self):
+        return self._price
+
+    @property
+    def quantity(self):
+        return self._quantity
