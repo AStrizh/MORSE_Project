fca40c984743da5feb3320478ccc55a513b98bdf
AStrizh <abstrizhevskiy@gmail.com>
2018-11-16 15:53:44 -0500

    Implemented Simple User interface

--- Main.py
+++ Main.py
@@ -2,21 +2,40 @@ import statistics
 import datetime
 import Trade
 import ReadTrades
+import ReadStats
 
 
 def main():
-    filename = "samplefile.txt"  # input("Input filename:")
+    fileinput = input("Input filename: ")
 
-    # trades = ReadTrades.read_record(filename)
-    #
-    # for trade in trades:
-    #     print(trade.time)
-    #     print(trade.price)
-    #     print(trade.quantity)
-    #     print("-----------------")
+    timeinput = input("Input a time: ")
+    print(ReadTrades.record_at_time(fileinput, timeinput))
 
-    price = ReadTrades.record_at_time(filename, "2017-05-19T05:49:34Z")
-    print(price)
+    timeinput1 = input("Input a start time: ")
+    timeinput2 = input("Input an end time: ")
+
+    try:
+        time1 = datetime.datetime.strptime(timeinput1.strip().replace('Z', ''), "%Y-%m-%dT%H:%M:%S")
+
+    except ValueError:
+        print("Cannot format Start time")
+        return
+
+    try:
+        time2 = datetime.datetime.strptime(timeinput2.strip().replace('Z', ''), "%Y-%m-%dT%H:%M:%S")
+
+    except ValueError:
+        print("Cannot format End time")
+        return
+
+    rs = ReadStats.ReadStats()
+    rs.read_stat_record(fileinput, time1, time2)
+
+    print("Max price was: " + str(rs.get_max()))
+    print("Min price was: " + str(rs.get_min()))
+    print("Average price was: " + str(rs.get_average()))
+    print("Standard Deviation: " + str(rs.get_div()))
+    print("Median Price: " + str(rs.get_median()))
 
 
 main()
